454493a24f687ebe39a7bd33578856417b17ca4b
Kemal Türk <kemal.turk@4alabs.io>
2023-09-02 01:44:09 +0300

    Add script to retrieve Docker credentials for AWS ECR Registry
    This commit adds a new shell script `run.sh` that retrieves the Docker credentials for the AWS ECR Registry. It sets up the necessary environment variables and uses the AWS CLI to get the login information. The script then creates a new secret in the specified namespace using `kubectl`.

---
+++ registry/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+set -e
+
+echo "Retrieving Docker Credentials for the AWS ECR Registry ${AWS_ACCOUNT}"
+DOCKER_REGISTRY_SERVER=https://${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com
+DOCKER_USER=AWS
+DOCKER_PASSWORD=`aws ecr get-login --region ${AWS_REGION} --registry-ids ${AWS_ACCOUNT} | cut -d' ' -f6`
+
+echo
+echo "Working in Namespace ${NAMESPACE_NAME}"
+echo
+echo "Removing previous secret in namespace ${NAMESPACE_NAME}"
+kubectl --namespace=${NAMESPACE_NAME} delete secret aws-registry || true
+
+echo "Creating new secret in namespace ${NAMESPACE_NAME}"
+kubectl create secret docker-registry aws-registry \
+	--docker-server=$DOCKER_REGISTRY_SERVER \
+	--docker-username=$DOCKER_USER \
+	--docker-password=$DOCKER_PASSWORD \
+	--docker-email=no@email.local \
+	--namespace=${NAMESPACE_NAME}
+echo
+echo
